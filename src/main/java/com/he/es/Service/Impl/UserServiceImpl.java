da49147489813c4d297676481aa85fdec53dd440
Ahhhnnn <36215241+Ahhhnnn@users.noreply.github.com>
2019-09-07 21:55:43 +0800

    test

---
+++ src/main/java/com/he/es/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.he.es.Service.Impl;
+
+/**
+ * @author heningm
+ * @description: TODO
+ * @date 2019/9/720:58
+ */
+public class UserServiceImpl {
+}
---
+++ src/main/java/com/he/es/Service/UserService.java
@@ -0,0 +1,4 @@
+package com.he.es.Service;
+
+public interface UserService {
+}
